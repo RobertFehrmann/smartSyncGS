747024759114ffd867a385ecd5818510e0cd99fc
Robert Fehrmann <RobertFehrmann2906@gmail.com>
2020-06-30 16:46:00 -0400

    new file

---
+++ metadata/sp_consolidate_gs.sql
@@ -0,0 +1,7 @@
+create or replace procedure METADATA.SP_CONSOLIDATE_GS(I_SRC_DB VARCHAR,I_TGT_DB VARCHAR, I_SCHEMA VARCHAR)
+    returns ARRAY
+    language JAVASCRIPT
+    execute as caller
+as
+$$
+$$;
